3f38a742389c3922de40a64d1feab66cb39e45f1
Joao Grassi <joao@tapkey.com>
2019-08-07 16:07:49 +0200

    inital commit

---
+++ src/WebApplication11/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication11
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Application> Applications { get; set; }
+
+        public AppContext(DbContextOptions<AppContext> options) 
+            : base(options)
+        {
+        }
+    }
+}
